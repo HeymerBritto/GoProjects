c8f045991000a70741fdc7a2e8af170eecdc374c
Heymer Britto <heymer_a7x@live.com>
2022-04-28 19:12:46 -0300

    First Commit

---
+++ HelloWorld/Main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
+
+//Create a project folder
+//Create Main.go File
+//Code
+
+//Go run .\Path\Main.go
+//Go build .\Path\
+//Go install .\Path and then exe will be created at bin folder on GOPATH
